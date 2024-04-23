434718faf3da131028ae113eeb8bb07df0408dd2
Aleksei Androsov <aandrosov@yandex-team.ru>
2024-04-23 20:22:41 +0300

    mocha -> vitest

---
+++ test/index.test.mjs
@@ -0,0 +1,31 @@
+import { join } from 'path';
+import { readdirSync, readFileSync } from 'fs';
+import { transformFileSync } from '@babel/core';
+import { describe, expect, it } from 'vitest';
+
+describe('fixtures', () => {
+    const fixturesDir = join(__dirname, 'fixtures');
+
+    readdirSync(fixturesDir).map((suiteName) => {
+        describe(suiteName, () => {
+            const suitePath = join(fixturesDir, suiteName);
+            readdirSync(suitePath).map((caseName) => {
+                if (caseName === '.babelrc') return;
+
+                it(caseName.split('-').join(' '), () => {
+                    const fixtureDir = join(suitePath, caseName);
+                    const actual     = transformFileSync(
+                        join(fixtureDir, 'actual.js')
+                    ).code;
+                    const expected = readFileSync(join(fixtureDir, 'expected.js')).toString();
+
+                    expect(actual.trim()).toEqual(expected.trim());
+                });
+            });
+        });
+    });
+});
+
+function trim(str) {
+    return str.replace(/^\s+|\s+$/, '');
+}
